61b2c627578d00bedc5055754632638f903b1f98
Dermot Fitzpatrick <39203833+tomredf@users.noreply.github.com>
2021-10-17 14:37:43 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,65 @@
+import turtle
+import pandas
+
+FONT = ("Arial", 10, "normal")
+
+screen = turtle.Screen()
+screen.title("US States Game")
+
+image = "blank_states_img.gif"
+screen.addshape(image)
+
+turtle.shape(image)
+
+data = pandas.read_csv("50_states.csv")
+
+
+t = turtle.Turtle()
+t.hideturtle()
+t.penup()
+
+
+def write_state(s, x, y):
+    t.goto(int(x), int(y))
+    t.write(s, align="center", font=FONT)
+
+
+correct_answers = []
+score = 0
+
+
+def check(s):
+    d = data[data.state.str.lower() == s]
+    global correct_answers
+    if d.empty:
+        print(f"Entered state {s} does not exist")
+    elif d.state.item() in correct_answers:
+        print(f"You already guessed {d.state.item()}")
+    else:
+        st = d.state.item()
+        x = d.x
+        y = d.y
+        write_state(st, x, y)
+        global score
+        score += 1
+
+        correct_answers.append(st)
+
+
+game_is_on = True
+
+while game_is_on:
+    if score == 50:
+        game_is_on = False
+
+    answer = screen.textinput(title=f"{score}/50 States Correct", prompt="What's another state name?")
+
+    if answer is None:
+        game_is_on = False
+        q = data.state.isin(correct_answers)
+        df = data[~q]
+        df.to_csv("missed_states.csv")
+    else:
+        check(answer.lower())
+
+
